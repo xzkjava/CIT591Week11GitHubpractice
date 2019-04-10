2f7b80db897e2c0881fb9940681a89b1b6ccdde3
snkm <snn@seas.upenn.edu>
2019-04-11 00:43:31 +0900

    Added comments

--- PracticeCode.java
+++ PracticeCode.java
@@ -1,13 +1,15 @@
 
 public class PracticeCode {
-	
+
 	public static void main(String[] args) {
 		double x = Math.sqrt(2);
 		double y = 2;
 		if (x * x == y) {
+			//they are equal
 			System.out.println("x * x and y are equal");
 		}
 		else {
+			//they are not equal
 			System.out.println("x * x and y are not equal");
 		}
 	}
