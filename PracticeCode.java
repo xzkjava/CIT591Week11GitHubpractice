9a17f1c2a11cb5af8fed64985b6376f3b00d3972
snkm <snn@seas.upenn.edu>
2019-04-10 23:49:32 +0900

    Added java file

---
+++ PracticeCode.java
@@ -0,0 +1,14 @@
+
+public class PracticeCode {
+	
+	public static void main(String[] args) {
+		double x = Math.sqrt(2);
+		double y = 2;
+		if (x * x == y) {
+			System.out.println("x * x and y are equal");
+		}
+		else {
+			System.out.println("x * x and y are not equal");
+		}
+	}
+}
